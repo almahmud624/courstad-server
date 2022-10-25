407ab0351b28a085d2533fafbaad5ace72d6a86f
almahmud624 <mahmudbhuiyan624@gmail.com>
2022-10-25 16:54:01 +0600

    data updated and make all courses data api

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 4000;
 
+const courses = require("./data/toturial.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Courstad Server is Running");
 });
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
 app.listen(port, () => {
   console.log("Courstad running on port", port);
 });
